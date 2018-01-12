851fc83a2355a352b38e48504d3e48ec8182cda0
Renan Henrique <henriquebasshvf@gmail.com>
2018-01-12 16:52:37 -0200

    Integração da API da Marvel

--- marvel-vue/src/App.vue
+++ marvel-vue/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="tabela-personagem">
+  <div id="tabela-personagens">
     <table class="table table-bordered">
       <thead>
        <th>Nome</th>
@@ -7,13 +7,10 @@
        <th>Detalhes</th>
       </thead>
       <tbody>
-      <td>
-        <tr v-for="personagem in personagens" :key="personagem.nome">{{ personagem.nome }}</tr>
-      </td>
-      <td>
-        <tr v-for="personagem in personagens" :key="personagem.nome">{{ personagem.descricao }}</tr>
-      </td>
-      <td></td>
+      <tr v-for="personagem in personagens" :key="personagem.name">
+        <td>{{ personagem.nome }}</td>
+        <td>{{ personagem.descricao }}</td>
+      </tr>
     </tbody>
   </table>
   </div>
@@ -24,16 +21,34 @@
 const baseUrl = 'https://gateway.marvel.com:443/v1/public'
 const apiKey = '1bf4c8e588d3f4faf897acf7b5ade1d5'
 
-const listarPersonagens = () => {
+const listarQuadrinhos = () => {
+
+}
+
+const quadrinhoPorNome = (nome) => {
+
+}
+
+export default {
+  name: "tabela-personagens",
+  data() {
+    return {
+     personagens: []
+    }
+  },
+
+  created () {
+  self = this;
   axios.get(`${baseUrl}/characters`, {
       params: {
         apikey: apiKey
       }
     })
     .then(function (response) {
-      return response.data.map((char) => {
+      console.log(response);
+       self.personagens = response.data.data.results.map((char) => {
         return {
-          name: char.name,
+          nome: char.name,
           descricao: char.description,
           imagem: char.thumbnail.path
         }
@@ -44,22 +59,6 @@ const listarPersonagens = () => {
     });
 }
 
-const listarQuadrinhos = () => {
-  
-}
-
-const quadrinhoPorNome = (nome) => {
-  
-}
-
-export default {
-  name: "tabela-personagens",
-  data() {
-    return {
-     personagens : listarPersonagens()
-    }
-  }
-  
 }
 </script>
 
