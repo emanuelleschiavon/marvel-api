26e28902ac074e6f8b11aad1e371aac5a800a8b2
Renan Henrique <henriquebasshvf@gmail.com>
2018-01-12 16:17:57 -0200

    Adicionado template da tabela com dados falsos

--- marvel-vue/src/App.vue
+++ marvel-vue/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <div id="app">
+  <div id="tabela-personagens">
+    <table>
+       <th>Nome</th>
+       <th>Descriçao</th>
+       <th>Detalhes</th>
+      <td>
+        <tr v-for="personagem in personagens">{{ personagem.nome }}</tr>
+      </td>
+      <td>
+        <tr v-for="personagem in personagens">{{ personagem.descricao }}</tr>
+      </td>
+    </table>
 
-    <form>
-      <input type="text" class="form-control" />
-    </form>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'tabela-personagens',
   data () {
     return {
+        personagens: [
+        { nome: 'Demolidor', descricao: 'Cego' },
+        { nome: 'Miss Marvel', descricao: 'Voa' },
+        { nome: 'Viuva Negra', descricao: 'Espia' },
+        { nome: 'Hulk', descricao: 'Verde' },
+        { nome: 'Homem de Ferro', descricao: 'Rico' }]
     }
-  },
-  // axios.get('/user?ID=12345')
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // })
+  }
 }
 </script>
 
--- marvel-vue/src/Personagens/personagem.vue
+++ marvel-vue/src/Personagens/personagem.vue
@@ -1,20 +1,37 @@
 <template>
-  <div id="app">
-
+  <div id="tabela-personagens">
+    <table>
+       <th>Nome</th>
+       <th>Descriçao</th>
+       <th>Detalhes</th>
+      <td>
+        <tr v-for="personagem in personagens">{{ personagem.nome }}</tr>
+      </td>
+      <td>
+        <tr v-for="personagem in personagens">{{ personagem.descricao }}</tr>
+      </td>
+    </table>
 
 
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-    }
+var app = new Vue({
+  el: '#tabela-personagens',
+  data: {
+    personagens: [
+      { nome: 'Demolidor', descricao: 'Cego' },
+      { nome: 'Miss Marvel', descricao: 'Voa' },
+      { nome: 'Viuva Negra', descricao: 'Espia' },
+      { nome: 'Hulk', descricao: 'Verde' },
+      { nome: 'Homem de Ferro', descricao: 'Rico' },
+    ]
   }
-}
+})
+
 </script>
 
 <style>
+
 </style>
