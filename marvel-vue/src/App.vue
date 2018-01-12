5671d85b65cec605e595bc4c036efeb34f4fd2d0
Renan Henrique <henriquebasshvf@gmail.com>
2018-01-12 16:27:24 -0200

    Colocado estilo na tabela e consertado erro key

--- marvel-vue/src/App.vue
+++ marvel-vue/src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <div id="tabela-personagens">
-    <table>
+  <div id="tabela-personagem">
+    <table class="table table-bordered">
+      <thead>
        <th>Nome</th>
        <th>Descriçao</th>
        <th>Detalhes</th>
+      </thead>
+      <tbody>
       <td>
-        <tr v-for="personagem in personagens">{{ personagem.nome }}</tr>
+        <tr v-for="personagem in personagens" :key="personagem.nome">{{ personagem.nome }}</tr>
       </td>
       <td>
-        <tr v-for="personagem in personagens">{{ personagem.descricao }}</tr>
+        <tr v-for="personagem in personagens" :key="personagem.nome">{{ personagem.descricao }}</tr>
       </td>
-    </table>
-
-
+      <td></td>
+    </tbody>
+  </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'tabela-personagens',
-  data () {
+  name: "tabela-personagens",
+  data() {
     return {
-        personagens: [
-        { nome: 'Demolidor', descricao: 'Cego' },
-        { nome: 'Miss Marvel', descricao: 'Voa' },
-        { nome: 'Viuva Negra', descricao: 'Espia' },
-        { nome: 'Hulk', descricao: 'Verde' },
-        { nome: 'Homem de Ferro', descricao: 'Rico' }]
-    }
+      personagens: [
+        { nome: "Demolidor", descricao: "Cego" },
+        { nome: "Miss Marvel", descricao: "Voa" },
+        { nome: "Viuva Negra", descricao: "Espia" },
+        { nome: "Hulk", descricao: "Verde" },
+        { nome: "Homem de Ferro", descricao: "Rico" }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
+
 </style>
