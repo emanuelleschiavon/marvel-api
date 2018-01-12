1c1c44c5e025d2e9ef2d510f44d33df69dbfb4a0
Renan Henrique <henriquebasshvf@gmail.com>
2018-01-12 15:38:39 -0200

    Commit inicial

---
+++ marvel-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+    <form>
+      <input type="text" class="form-control" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  // axios.get('/user?ID=12345')
+  // .then(function (response) {
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // })
+}
+</script>
+
+<style>
+</style>
